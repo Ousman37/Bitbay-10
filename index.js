47c01accc5fc2ac610cdcd808cb2b170d3ffa92b
Ethmane Didi <ousman.didibela@gmail.com>
2023-03-03 22:42:35 +0100

    commit index.js

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
-const core = require('@actions/core');/Users/didibilalethmane/BidBay-Platform/.eslintignore
-const github = require('@actions/github');
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const port = process.argv[2] || 1117;
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput('time', time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+http
+  .createServer(function (req, res) {
+    console.log(`${req.method} ${req.url}`);
+
+    // parse URL
+    const parsedUrl = url.parse(req.url);
+    // extract URL path
+    let pathname = `.${parsedUrl.pathname}`;
+    // based on the URL path, extract the file extension. e.g. .js, .doc, ...
+    const ext = path.parse(pathname).ext;
+    // maps file extension to MIME typere
+    const map = {
+      '.ico': 'image/x-icon',
+      '.html': 'text/html',
+      '.js': 'text/javascript',
+      '.json': 'application/json',
+      '.css': 'text/css',
+      '.png': 'image/png',
+      '.jpg': 'image/jpeg',
+      '.wav': 'audio/wav',
+      '.mp3': 'audio/mpeg',
+      '.svg': 'image/svg+xml',
+      '.pdf': 'application/pdf',
+      '.doc': 'application/msword',
+    };
+
+    fs.exists(pathname, function (exist) {
+      if (!exist) {
+        // if the file is not found, return 404
+        res.statusCode = 404;
+        res.end(`File ${pathname} not found!`);
+        return;
+      }
+
+      // if is a directory search for index file matching the extension
+      if (fs.statSync(pathname).isDirectory()) pathname += '/index.html' + ext;
+
+      // read file from file system
+      fs.readFile(pathname, function (err, data) {
+        if (err) {
+          res.statusCode = 500;
+          res.end(`Error getting the file: ${err}.`);
+        } else {
+          // if the file is found, set Content-type and send data
+          res.setHeader('Content-type', map[ext] || 'text/html');
+          res.end(data);
+        }
+      });
+    });
+  })
+  .listen(parseInt(port));
+
+console.log(`Server listening on port ${port}`);
